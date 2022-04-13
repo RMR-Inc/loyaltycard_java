06398c9bbe1fe6e92c6c13d1e01f483a0867e1ae
DynamoRed <raphael@dynamored.com>
2022-04-13 08:44:10 +0200

    Base project

---
+++ src/main/java/fr/rmrinc/loyaltycard/App.java
@@ -0,0 +1,12 @@
+package fr.rmrinc.loyaltycard;
+
+/**
+ * @author RMR-Inc
+ * @developer DynamoRed
+ * 2022
+ */
+public class App {
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/fr/rmrinc/loyaltycard/AppTest.java
@@ -0,0 +1,18 @@
+package fr.rmrinc.loyaltycard;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author RMR-Inc
+ * @developer DynamoRed
+ * 2022
+ */
+public class AppTest {
+    
+    @Test
+    public void shouldAnswerWithTrue(){
+        assertTrue(true);
+    }
+}
